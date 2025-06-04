4fa4e53449310aa83d027f7a804de082bc161df2
unknown <dkshatree88@gmail.com>
2025-06-04 13:40:26 +0545

    updating db setting

--- interview_app/settings.py
+++ interview_app/settings.py
@@ -72,16 +72,16 @@ WSGI_APPLICATION = "interview_app.wsgi.application"
 
 
 
-# DATABASES = {
-#     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
